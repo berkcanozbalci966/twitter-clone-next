8fa67368a6c44fa1d8199ecbfc4799858519c8ef
unknown <berkcanozbalci@gmail.com>
2022-02-28 00:37:50 +0300

    feat: sidebar completed

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -15,10 +15,10 @@ import SidebarLink from "./SidebarLink";
 export default function Sidebar() {
   return (
     <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
-      <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24 hover-animation">
+      <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
         <Image src="https://rb.gy/ogau5a" width={30} height={30} />
       </div>
-      <div className="space-y-2 5 mt-4 mb-2.5 xl:ml-24">
+      <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
         <SidebarLink text="Home" Icon={HomeIcon} active />
         <SidebarLink text="Explore" Icon={HashtagIcon} />
         <SidebarLink text="Notifications" Icon={BellIcon} />
@@ -27,6 +27,23 @@ export default function Sidebar() {
         <SidebarLink text="Lists" Icon={ClipboardListIcon} />
         <SidebarLink text="Profile" Icon={UserIcon} />
         <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
+      </div>
+      <button className="hidden xl:inline ml-auto bg-[#1d9bf0] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]">
+        Tweet
+      </button>
+      <div className="text-[#d9d9d9] flex items-center justify-center mt-auto hoverAnimation xl:ml-auto xl:-mr-5">
+        <img
+          src={
+            "https://avatars.githubusercontent.com/u/54132791?s=400&u=279884898037b7ee7c04697e885ba34dd6c0a679&v=4"
+          }
+          alt=""
+          className="h-10 w-10 rounded-full xl:mr-2.5"
+        />
+        <div className="hidden xl:inline leading-5">
+          <h4 className="font-bold">Berkcan</h4>
+          <p className="text-[#6e767d]">@berkcan</p>
+        </div>
+        <DotsHorizontalIcon className="h-5 hidden xl:inline ml-10" />
       </div>
     </div>
   );
--- components/SidebarLink.js
+++ components/SidebarLink.js
@@ -3,10 +3,12 @@ import React from "react";
 export default function SidebarLink({ Icon, text, activate }) {
   return (
     <div
-      className={`text-[#d9d9d9] flex items-center justify-center xl:justify-start text-xl space-x-3 hover-animation`}
+      className={`text-[#d9d9d9] flex items-center justify-center xl:justify-start text-xl space-x-3 hover-animation ${
+        activate && "font-bold"
+      } `}
     >
       <Icon className="h-7 text-white" />
-      <span>{text}</span>
+      <span className="hidden xl:inline">{text}</span>
     </div>
   );
 }
