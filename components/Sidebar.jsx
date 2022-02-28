76a217be35ab23da248194e44ce6893264a69bb6
unknown <berkcanozbalci@gmail.com>
2022-02-28 09:15:02 +0300

    feat: Feed component created

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -16,7 +16,7 @@ export default function Sidebar() {
   return (
     <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
       <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
-        <Image src="https://rb.gy/ogau5a" width={30} height={30} />
+        <Image src="https://rb.gy/ogau5a" width={30} height={30} alt="" />
       </div>
       <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
         <SidebarLink text="Home" Icon={HomeIcon} active />
@@ -32,12 +32,14 @@ export default function Sidebar() {
         Tweet
       </button>
       <div className="text-[#d9d9d9] flex items-center justify-center mt-auto hoverAnimation xl:ml-auto xl:-mr-5">
-        <img
+        <Image
           src={
             "https://avatars.githubusercontent.com/u/54132791?s=400&u=279884898037b7ee7c04697e885ba34dd6c0a679&v=4"
           }
           alt=""
           className="h-10 w-10 rounded-full xl:mr-2.5"
+          width={10}
+          height={10}
         />
         <div className="hidden xl:inline leading-5">
           <h4 className="font-bold">Berkcan</h4>
